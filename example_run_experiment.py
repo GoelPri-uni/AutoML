5418f901314c9a920790971c36301614b9fbf20f
SandraStrakova <46136738+SandraStrakova@users.noreply.github.com>
2024-10-18 13:23:26 +0200

    Added new datasets to the first question

--- example_run_experiment.py
+++ example_run_experiment.py
@@ -9,7 +9,8 @@ from surrogate_model import SurrogateModel
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--config_space_file', type=str, default='lcdb_config_space_knn.json')
-    parser.add_argument('--configurations_performance_file', type=str, default='lcdb_configs.csv')
+    parser.add_argument('--configurations_performance_file', type=list, default= ['lcdb_configs.csv', 'config_performances_dataset-6.csv', 'config_performances_dataset-11.csv', 'config_performances_dataset-1457.csv'])
+
     # max_anchor_size: connected to the configurations_performance_file. The max value upon which anchors are sampled
     parser.add_argument('--max_anchor_size', type=int, default=1600)
     parser.add_argument('--num_iterations', type=int, default=500)
@@ -18,29 +19,37 @@ def parse_args():
 
 
 def run(args):
-    config_space = ConfigSpace.ConfigurationSpace.from_json(args.config_space_file)
-    random_search = RandomSearch(config_space)
-    df = pd.read_csv(args.configurations_performance_file)
-    surrogate_model = SurrogateModel(config_space)
-    #surrogate_model.fit(df)
-    surrogate_model.fit(df[df['anchor_size'] == 1600])
-    results = {
-        'random_search': [1.0]   #add a function to find the points from where its the least performance 
-    }
-
-    for idx in range(args.num_iterations):
-        theta_new = dict(random_search.select_configuration())
-        
-        theta_new['anchor_size'] = args.max_anchor_size
-        performance = surrogate_model.predict(theta_new)
-        # ensure to only record improvements
+    for dataset in args.configurations_performance_file:
        
-        results['random_search'].append(min(results['random_search'][-1], performance))
-        random_search.update_runs((theta_new, performance))
+        config_space = ConfigSpace.ConfigurationSpace.from_json(args.config_space_file)
+        config_space.seed(42) #fixing the random seed for plot reproducibility
+        random_search = RandomSearch(config_space)
+        df = pd.read_csv(dataset) #(args.configurations_performance_file)
+        surrogate_model = SurrogateModel(config_space)
+        #surrogate_model.fit(df)
+        max_anchor_size = max(df['anchor_size'].unique())
+        print('max anchor size', max_anchor_size)
+        surrogate_model.fit(df[df['anchor_size'] == max_anchor_size])
+        results = {
+            'random_search': [1.0]   
+        }
 
-    plt.plot(range(len(results['random_search'])), results['random_search'])
-    plt.yscale('log')
-    plt.show()
+        for idx in range(args.num_iterations):
+            theta_new = dict(random_search.select_configuration())
+            
+            theta_new['anchor_size'] = args.max_anchor_size
+            performance = surrogate_model.predict(theta_new)
+            # ensure to only record improvements
+        
+            results['random_search'].append(min(results['random_search'][-1], performance))
+            random_search.update_runs((theta_new, performance))
+
+        plt.plot(range(len(results['random_search'])), results['random_search'])
+        plt.yscale('log')
+        plt.xlabel('Num iterations')
+        plt.ylabel('Score')
+        plt.title('Surrogate model score predictions with max_anchor size of ' + str(max_anchor_size))
+        plt.show()
 
 
 if __name__ == '__main__':
